75a7cf1b1ae0cb6d2ced8abf3143429a684b1afe
Felipe Fischel <107649745+felipefischel@users.noreply.github.com>
2022-10-03 21:39:17 +0000

    created the form in the index html and created the method that returns the trip info

--- TransportationComparison/urls.py
+++ TransportationComparison/urls.py
@@ -7,5 +7,5 @@ app_name = 'comparison'
 urlpatterns =[
   path('',views.Index, name='index'),
   path('result/<int:trip_output_id>', views.Result, name='result'),
-  path('compare/<int:trip_input_id>',views.Compare,name='compare')
+  path('compare/',views.Compare,name='compare')
 ]
--- TransportationComparison/views.py
+++ TransportationComparison/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from .models import TripOutput
+from django.http import Http404
 
 
 # Create your views here.
@@ -10,30 +12,36 @@ from django.urls import reverse
 def Index(request):
     template = loader.get_template('index.html')
     context = {}
-    
+
     return HttpResponse(template.render(context, request))
 
 
-def Result(request, tripOutput_id):
+def Result(request, trip_output_id):
+    try:
+      trip = TripOutput.objects.get(pk=trip_output_id)
+    except TripOutput.DoesNotExist:
+      raise Http404("Trip does not exist")
+  
     template = loader.get_template('result.html')
-    #leemos la base de datos
+    #leemos la base de datos y la guardamos en una variable X
     context = {
-        # bla bla
-        #TripOutput
+        "flightDuration":trip.flight_duration,
+        "driveDuration":trip.drive_duration,
+        "flightCost":trip.flight_cost,
+        "driveCost":trip.drive_cost
     }
     return HttpResponse(template.render(context, request))
 
 
-def Compare(request,trip_input_id):
+def Compare(request):
     title = request.POST['title']
-    #este resive un Post
-    #logica que convierte un Comparison input a Comparison Output
-    #guardamos el Comparison Output en base de datos
-    #el key del nuevo entry va a ser ComparisonOutput.id
+    starting_destination = request.POST['starting_destination']
+    #hacer los api calls;
+
+    #guardar el resultado en un TripOutput con key = tripOutput_id;
+
+    #redireccionar a Result pasando el valor de tripOutput_id;
 
     #return HttpResponseRedirect(reverse('comparison:results', args=(trip_output.id,)))
     print(title)
-    return HttpResponseRedirect(reverse('comparison:result', args=(1,)))
-
-
-
+    return HttpResponseRedirect(reverse('comparison:result', args=(1, )))
